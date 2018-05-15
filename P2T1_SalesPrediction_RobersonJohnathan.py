f2874132be417be0e4a22f5be8b630a4c807adfd
robersoj7042 <36608637+robersoj7042@users.noreply.github.com>
2018-05-15 18:17:55 -0400

    Python 2
    P2T1- Sales Prediction, P2HW1- Distance Traveled, and P2HW2- Tip Tax Total

---
+++ P2T1_SalesPrediction_RobersonJohnathan.py
@@ -0,0 +1,19 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #CTI-110
+>>> #P2T1 - Sales Prediction
+>>> #Johnathan Roberson
+>>> #02/16/2018
+>>> 
+ RESTART: C:/Users/johnr/OneDrive/Documents/FTCC Homework/P2T1_sales/P2T1_SalesPrediction_RobersonJohnathan.py 
+Enter the projected sales: 1000
+The profit is $ 230.0
+>>> 
+ RESTART: C:/Users/johnr/OneDrive/Documents/FTCC Homework/P2T1_sales/P2T1_SalesPrediction_RobersonJohnathan.py 
+Enter the projected sales: 1000
+The profit is $ 230.00
+>>> 
+ RESTART: C:/Users/johnr/OneDrive/Documents/FTCC Homework/P2T1_sales/P2T1_SalesPrediction_RobersonJohnathan.py 
+Enter the projected sales: 10000
+The profit is $ 2,300.00
+>>> 
